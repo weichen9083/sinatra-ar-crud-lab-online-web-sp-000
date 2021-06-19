91ed89a40cc7fb38b62e0d1cb98287f3a3507bff
wei chen <weichen9083@gmail.com>
2021-06-19 04:06:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,4 +40,9 @@ class ApplicationController < Sinatra::Base
       redirect to "/articles/#{ @article.id }"
     end
     
+     delete '/articles/:id' do
+      Article.delete(params[:id])
+      redirect to "/articles"
+    end
+    
 end

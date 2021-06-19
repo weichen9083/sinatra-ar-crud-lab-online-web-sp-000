c705a3155bb6927896ed88989158cc7821f5684c
wei chen <weichen9083@gmail.com>
2021-06-19 04:05:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,8 @@ class ApplicationController < Sinatra::Base
     end 
     
     post '/articles' do 
+      @article = Article.create(title: params[:title], content: params[:content])
+      redirect to "/articles/#{@article.id}"
       
     end 
     
